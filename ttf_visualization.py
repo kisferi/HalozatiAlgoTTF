57231bf8df03f422c9f29756738462df348cc290
Kis Ferenc <ferenc.kis@prefixbox.com>
2021-04-20 22:11:02 +0200

    visualisation base works

--- ttf_visualization.py
+++ ttf_visualization.py
@@ -17,12 +17,14 @@ class TTFVisualizer:
         G = nx.MultiDiGraph()
         colorMap=[]
         for i in range(0, self.ttfObject.numberOfAgents):
+            G.add_node(i)
             if i==0:
-                G.add_node(i)
                 colorMap.append('red')
             else:
-                G.add_node(i)
                 colorMap.append('blue')
+
+        pos = nx.circular_layout(G)
+        plt.ion()
         interactions=[]
         while self.ttfObject.is_termination_configuration() != True:
             currentInteraction = self.ttfObject.next_interaction()
@@ -30,25 +32,15 @@ class TTFVisualizer:
             numberOfInteractions += 1
             print('Number of interactions: ', numberOfInteractions)
             print('TokenList: ', self.ttfObject.tokenList)
-            edge_colors = []
-            for i in interactions:
-                edge_colors.append('black')
-            interactions.append(currentInteraction[0])
-            edge_colors.append('red')
+
             print("Interactions: ",interactions)
-            G.add_edges_from(interactions)
-            print('nodes',G.nodes)
-            
-            
 
-
-            plt.figure(figsize=(8,8))
+            G.add_edge(currentInteraction[0][0], currentInteraction[0][1])
             
-            nx.draw(G,with_labels=True, node_color = colorMap, edge_color = edge_colors)
-
-            if self.ttfObject.is_termination_configuration() != True:
-                plt.show(block=False)
-                plt.pause(3)
-                plt.close()
-            else:
+            nx.draw(G,with_labels=True, node_color = colorMap, pos= pos)
+            if not self.ttfObject.is_termination_configuration():
+                plt.pause(0.1)
                 plt.show()
+                plt.clf()
+                
+           
