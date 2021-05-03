293e082eadb7425944c55720a568eb7294069229
Kis Ferenc <ferenc.kis@prefixbox.com>
2021-05-03 20:23:23 +0200

    curved edges and delete next transaction button

--- ttf_visualization.py
+++ ttf_visualization.py
@@ -14,10 +14,8 @@ class TTFVisualizer:
     outter_node_lables = {}
     outter_node_lable_pos = {}
     node_positions = []
-    edeg_colors = []
-    edges = []
     interactions = []
-    G = nx.Graph()
+    G = nx.MultiGraph()
     ax = []
     run_btn = {}
     next_trans_btn = {}
@@ -29,48 +27,55 @@ class TTFVisualizer:
         #set the graph
         self.G.add_nodes_from(range(0,self.ttfObject.numberOfAgents))
         self.node_positions = nx.spring_layout(self.G)
-        #add all edges to the graph
-        for i in range(self.ttfObject.numberOfAgents):
-            for j in range(self.ttfObject.numberOfAgents):
-                if(i<j):
-                    self.G.add_edge(i,j)
-                    self.edges.append((i,j))
-                    self.edeg_colors.append('black')
         self.set_node_outter_lables()
 
     def run_ttf(self):
         #turn on pyplot interactive mode
 
-        fig, axes = plt.subplots(nrows=2, ncols=1, gridspec_kw={'height_ratios': [9, 1]})
+        fig, axes = plt.subplots(nrows=2, ncols=1, gridspec_kw={'height_ratios': [20, 1]})
         self.ax = axes.flatten()
         self.ax[1].set_axis_off()
         plt.tight_layout()
         #set the node colors
         self.set_node_colors()
         self.set_node_labels()
-        nx.draw(self.G, ax = self.ax[0] ,with_labels=True, node_color = self.node_colors, pos = self.node_positions, labels = self.node_labels, edge_color = self.edeg_colors, linewidths=4, font_size=16,node_size=250,  width=2)
+        nx.draw(self.G, ax = self.ax[0] ,with_labels=True, node_color = self.node_colors, pos = self.node_positions, labels = self.node_labels, linewidths=4, font_size=16,node_size=250,  width=2, connectionstyle="arc3,rad=0.3")
         nx.draw_networkx_labels(self.G,ax= self.ax[0], pos = self.outter_node_lable_pos, labels=self.outter_node_lables)
 
+        for i in range(self.ttfObject.numberOfAgents):
+            for j in range(self.ttfObject.numberOfAgents):
+                source, target = i,j
+                arrowprops=dict(arrowstyle="<|-", color="black",
+                                shrinkA=15, shrinkB=15,
+                                patchA=None, patchB=None,
+                                connectionstyle="arc3,rad=0.1",
+                                )
+                self.ax[0].annotate("",
+                            xy=self.node_positions[source],
+                            xytext=self.node_positions[target],
+                            arrowprops=arrowprops
+                        )
+
         #run button
-        axcut = plt.axes([0.01, 0.1, 0.1, 0.075])
+        axcut = plt.axes([0.01, 0.002, 0.1, 0.075])
         self.run_btn = Button(axcut, 'Run')
         self.run_btn.on_clicked(self.run)
 
         #next button
-        axcut = plt.axes([0.12, 0.1, 0.1, 0.075])
+        axcut = plt.axes([0.12, 0.002, 0.1, 0.075])
         self.next_btn = Button(axcut, 'Next')
         self.next_btn.on_clicked(self.next)
 
-        #next data transfer button
-        axcut = plt.axes([0.23, 0.1, 0.2, 0.075])
-        self.next_trans_btn = Button(axcut, 'Next Transfer')
-        self.next_trans_btn.on_clicked(self.run)
-        
+        # #next data transfer button
+        # axcut = plt.axes([0.23, 0.002, 0.2, 0.075])
+        # self.next_trans_btn = Button(axcut, 'Next Transfer')
+        # self.next_trans_btn.on_clicked(self.run)
+
         #add text with time and energy consumed
-        axcut = plt.axes([0.63, 0.1, 0.1, 0.075])
+        axcut = plt.axes([0.63, 0.002, 0.1, 0.075])
         self.text_box1 = TextBox(axcut, "Time: ")
         self.text_box1.set_val(str(self.index))
-        axcut = plt.axes([0.83, 0.1, 0.1, 0.075])
+        axcut = plt.axes([0.83, 0.002, 0.1, 0.075])
         self.text_box2 = TextBox(axcut, "Energy: ")
         self.text_box2.set_val(str(self.ttfObject.numberOfDataTransitions))
         plt.show()
@@ -81,18 +86,18 @@ class TTFVisualizer:
             self.interactions.append(self.ttfObject.next_interaction())
             print('Current interaction: ', self.interactions[self.index])
             print('Number of interactions: ', self.index)
-            
+
             #set the node colors
             self.set_node_colors()
             self.set_node_labels()
             self.set_sender_reciver_colors(self.index)
             self.set_interaction_edge_color(self.index)
 
-            nx.draw(G=self.G,ax = self.ax[0] ,with_labels=True, node_color = self.node_colors, pos = self.node_positions, labels = self.node_labels, edge_color = self.edeg_colors, linewidths=4, font_size=16,node_size=250,  width=2)
+            nx.draw(G=self.G,ax = self.ax[0] ,with_labels=True, node_color = self.node_colors, pos = self.node_positions, labels = self.node_labels, linewidths=4, font_size=16,node_size=250,  width=2)
             nx.draw_networkx_labels(self.G,ax= self.ax[0], pos = self.outter_node_lable_pos, labels=self.outter_node_lables)
 
             if not self.ttfObject.is_termination_configuration():
-                plt.pause(0.6)
+                plt.pause(0.2)
                 self.text_box2.set_val(str(self.ttfObject.numberOfDataTransitions))
             # #increase the interactions number
                 self.index += 1
@@ -102,7 +107,7 @@ class TTFVisualizer:
             else:
                 print("Done")
                 plt.show(block=True)
-            
+
 
     def next(self, event):
         if not self.ttfObject.is_termination_configuration():
@@ -117,7 +122,7 @@ class TTFVisualizer:
             self.set_sender_reciver_colors(self.index)
             self.set_interaction_edge_color(self.index)
 
-            nx.draw(self.G,ax = self.ax[0] ,with_labels=True, node_color = self.node_colors, pos = self.node_positions, labels = self.node_labels, edge_color = self.edeg_colors, linewidths=4, font_size=16,node_size=250,  width=2)
+            nx.draw(self.G,ax = self.ax[0] ,with_labels=True, node_color = self.node_colors, pos = self.node_positions, labels = self.node_labels, font_size=16,node_size=250,  width=2)
             nx.draw_networkx_labels(self.G,ax= self.ax[0], pos = self.outter_node_lable_pos, labels=self.outter_node_lables)
             self.text_box2.set_val(str(self.ttfObject.numberOfDataTransitions))
             self.index += 1
@@ -155,14 +160,22 @@ class TTFVisualizer:
             k +=1
 
     def set_interaction_edge_color(self, interactionIndex):
-        self.edeg_colors[0:] = ['black'] * len(self.edeg_colors)
-        sender, reciver, communication = self.interactions[interactionIndex][0][1],self.interactions[interactionIndex][0][0], self.interactions[interactionIndex][1]
-        index = -1
-        if (sender, reciver) in self.edges:
-            index = self.edges.index((sender,reciver))
-        else:
-            index = self.edges.index((reciver,sender))
-        if communication:
-            self.edeg_colors[index]= 'green'
-        else:
-            self.edeg_colors[index] = 'red'
+        for i in range(self.ttfObject.numberOfAgents):
+                for j in range(self.ttfObject.numberOfAgents):
+                    source, target = i,j
+                    color = 'black'
+                    if source == self.interactions[self.index][0][1] and target == self.interactions[self.index][0][0]:
+                        if self.interactions[self.index][1]:
+                            color= 'green'
+                        else:
+                            color = 'red'
+                    arrowprops=dict(arrowstyle="<|-", color=color,
+                                    shrinkA=15, shrinkB=15,
+                                    patchA=None, patchB=None,
+                                    connectionstyle="arc3,rad=0.1",
+                                    )
+                    self.ax[0].annotate('',
+                                xy=self.node_positions[source],
+                                xytext=self.node_positions[target],
+                                arrowprops=arrowprops
+                            )
